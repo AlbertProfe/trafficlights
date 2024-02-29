a0e2ffb06d74e35c1fc2ef1f9211a1dd4b35e992
albert <albertprofe>
2024-02-29 17:15:07 +0100

    consolidate and merge state into crossing hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-// Generate a random integer within the specified range,
-//inclusive of both minimum and maximum values.
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Returns a random color from a predefined array of colors.
 function getRandomColor() {
   const colors = ["red", "blue", "green", "yellow", "purple", "orange"];
   return colors[getRandomInt(0, colors.length - 1)];
@@ -17,49 +14,51 @@ function refreshPage() {
 }
 
 export default function TrafficLightSimulation() {
-  //const peopleToWalk = getRandomInt(10, 20);
   const peopleToWalk = 12;
   const initGroupWalking = getRandomInt(2, 6);
 
-  const [pending, setPending] = useState(peopleToWalk);
-  const [walking, setWalking] = useState(0);
-  const [completed, setCompleted] = useState(0);
-  const [groupWalking, setGroupWalking] = useState(initGroupWalking);
-  const [light, setLight] = useState(false);
+  const [crossing, setCrossing] = useState({
+    pending: peopleToWalk,
+    walking: 0,
+    completed: 0,
+    groupWalking: initGroupWalking,
+    light: false,
+  });
 
-  // setInterval function repeatedly calls a function or executes a code snippet,
-  // with a fixed time delay between each call.
-  // It takes two parameters: a function to be executed and
-  // the interval (in milliseconds) on how frequently the function should be called.
-
-  // clearInterval function stops the repeated execution
-  // of the function specified by setInterval.
-  // It takes the interval ID returned by setInterval as its parameter.
-  // When clearInterval is called with the interval ID, it clears the interval,
-  // preventing further executions of the specified function.
   function handleTrafficLightClick() {
-    setLight(true); // Turn on the traffic light
-    let walked = 0; // Initialize walked variable to count person walked
+    setCrossing((prevState) => ({
+      ...prevState,
+      light: true,
+    }));
+
+    let walked = 0;
     let maxToWalk = 0;
     const walkingInterval = setInterval(() => {
+      const { pending, groupWalking } = crossing;
       if (groupWalking >= pending) maxToWalk = pending;
       else maxToWalk = groupWalking;
-      // Check if there are pending person and
-      //if the maximum person limit hasn't been reached
+
       if (walked < maxToWalk) {
-        // Increment/decrement  the walking, pending and walked variables
-        setWalking((w) => w + 1);
-        setPending((p) => p - 1);
+        setCrossing((prevState) => ({
+          ...prevState,
+          walking: prevState.walking + 1,
+          pending: prevState.pending - 1,
+        }));
         walked++;
       } else {
-        clearInterval(walkingInterval); // Stop the walking interval
-        setCompleted((c) => c + walked); // Add the walked count to the total completed
-        setWalking(0); // Reset the walking count
-        setLight(false); // Turn off the traffic light
-        setGroupWalking(initGroupWalking);
+        clearInterval(walkingInterval);
+        setCrossing((prevState) => ({
+          ...prevState,
+          completed: prevState.completed + walked,
+          walking: 0,
+          light: false,
+          groupWalking: initGroupWalking,
+        }));
       }
-    }, 1000); // Adjust the delay as needed (1 second interval)
+    }, 1000);
   }
+
+  const { pending, walking, completed, groupWalking, light } = crossing;
 
   return (
     <>
