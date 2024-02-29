5f1f7ede43bbd7b176a5ff4ec0f749f3a2255505
albert <albertprofe>
2024-02-29 16:58:32 +0100

    delaySyncTime call function

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,23 @@ export default function RequestTracker() {
   const [completed, setCompleted] = useState(0);
   const [light, setLight] = useState(false);
 
-  /*
-  useEffect(() => {
-    console.log("pending effect:", pending);
-  }, [pending]);
-  */
-
   function handleTrafficLightClick() {
-    //console.log("before", pending);
     setPending((p) => p - 10);
-    //console.log("after", pending);
     setWalking((w) => w + 10);
-    setLight(true); // Set light to green when walking starts
-    setTimeout(() => {
-      setWalking((w) => w - 10);
-      setCompleted((c) => c + 10);
-      setLight(false); // Set light to red when walking is completed
-    }, 5000); // Delay for 3000 milliseconds (5 seconds)
+    setLight((l) => !l);
+    delaySyncTime(3000) // Delay for 3000 milliseconds (3 seconds)
+      .then(() => {
+        setWalking((w) => w - 10);
+        setCompleted((c) => c + 10);
+        setLight((l) => !l);
+      });
   }
 
   return (
     <>
       <h3 style={{ color: light ? "grey" : "white" }}> Pending: {pending} </h3>
       <h3 style={{ color: light ? "white" : "grey" }}>
-        Walking: {walking} {light ? " . . . . ." : ""}
+        Walking: {walking} {light ? " . . ." : ""}
       </h3>
       <h3 style={{ color: light ? "green" : "grey" }}>
         Completed: {completed}
@@ -51,3 +44,8 @@ export default function RequestTracker() {
   );
 }
 
+function delaySyncTime(timeInMilliseconds) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeInMilliseconds);
+  });
+}
